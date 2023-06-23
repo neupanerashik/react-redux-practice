e55ed3d9949bcf7fe8505693dbf311a1686e42ff
Rashik Neupane <neupanerashik1@gmail.com>
2023-06-23 23:31:06 +0545

    action-creators, reducers

--- src/Shop.js
+++ src/Shop.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Shop = () => {
   return (
     <div>
-        <h2>Adidas Shoes</h2>
-        <h4>Rs.50/-</h4>
+        <h2>Deposit/Withdraw Balance</h2>
+        {/* <h4>Rs.50/-</h4> */}
         <button className='btn btn-primary mx-2'> - </button>
-        Add to Cart
+        Update Balance
         <button className='btn btn-primary mx-2'> + </button>
     </div>
   )
